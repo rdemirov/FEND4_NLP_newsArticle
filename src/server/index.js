ac99e58de74ede1049504f0768dbdcf04534478a
Radoslav Demirov <rdemirov@gmail.com>
2020-01-19 17:57:46 +0200

    feat: Complete server part
    - Make application port dynamic
    - Add API settings
    - Call Aylien API using the supplied URL parameter

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,41 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const ailyenAPI = require("aylien_textapi");
 
-const app = express()
+const PORT = process.env.APP_PORT || 8000;
 
-app.use(express.static('dist'))
+const textProcessingAPI = new ailyenAPI({
+  application_id: process.env.APP_ID, //c64792a5
+  application_key: process.env.API_KEY //ca74fd266980aa04e453fdbf68349846
+});
 
-console.log(__dirname)
+const app = express();
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+app.use(express.static("dist"));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile("dist/index.html");
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(PORT, function() {
+  console.log(`Sentiment Analysis server listening on port ${PORT}`);
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.post("/analyzeSentiment", function(req, resp) {
+  textProcessingAPI.sentiment(
+    {
+      url: req.body.url
+    },
+    function(err, res) {
+      console.log(res);
+      if (!err) resp.send(res);
+      else resp.send("Error analyzing sentiment.Please try again.");
+    }
+  );
+});
